1341bed4f7298794adc4fad9a670519a72b11cac
AtharvaPanegai <panegai.atharv@gmail.com>
2022-01-02 18:09:33 +0530

    Url encoded form data

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/myget", (req, res) => {
+  console.log(req.body);
+
+  res.send(req.body);
+});
 
 app.listen(4000, () => {
   console.log("Server is Runnning at 4000");
